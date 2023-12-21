3f8a69b22dc8563c30fb613cd2a0b72c3221528e
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-12-21 15:20:06 +0100

    create database and train table

---
+++ database/migrations/2023_12_21_131622_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('departure_station',255);
+            $table->string('arrival_station',255);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_number',6);
+            $table->tinyInteger('wagons')->unsigned();
+            $table->tinyInteger('in_time')->default(1)->unsigned();
+            $table->tinyInteger('deleted')->default(0)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
